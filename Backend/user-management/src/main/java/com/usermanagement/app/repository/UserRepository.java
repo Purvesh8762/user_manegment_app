9c9ad2ab7b20f63b21ed21e5a720596849d138d6
Purvesh8762 <purveshsawalakhe8762@gmail.com>
2026-01-08 10:32:33 +0530

    Add files via upload

---
+++ Backend/user-management/src/main/java/com/usermanagement/app/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.usermanagement.app.repository;
+
+import com.usermanagement.app.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
